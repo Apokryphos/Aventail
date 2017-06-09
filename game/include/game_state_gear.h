77869482faee61ad33c766f7ef70ee4d5d87c9f3
Tony Slavotinek <apokryphos@users.noreply.github.com>
2017-06-08 22:37:49 -0400

    Changed some names from camel-case to underscored.

--- game/include/game_state_gear.h
+++ game/include/game_state_gear.h
@@ -3,7 +3,7 @@
 
 struct Game;
 
-void draw_gear_game_state(struct Game* game, int inTransition);
+void draw_gear_game_state(struct Game* game, int in_transition);
 void activate_gear_game_state(struct Game* game);
 void deactivate_gear_game_state(struct Game* game);
 void update_gear_game_state(struct Game* game);
--- game/src/game_state_main_menu.c
+++ game/src/game_state_main_menu.c
@@ -39,47 +39,47 @@ static void init_menu_gui_screen()
 {
     menu_gui_screen = create_gui_screen();
 
-    const char* titleText = "Aventail";
-    int titleWidth, titleHeight;
-    measure_text(titleText, &titleWidth, &titleHeight);
+    const char* title_text = "Aventail";
+    int title_width, title_height;
+    measure_text(title_text, &title_width, &title_height);
 
-    struct Panel* titlePanel = create_panel(NULL, PANEL_BORDER_STYLE_NONE);
-    titlePanel->text = titleText;
-    titlePanel->width = titleWidth;
-    titlePanel->height = titleHeight;
-    add_panel_to_gui_screen(menu_gui_screen, titlePanel);
+    struct Panel* title_panel = create_panel(NULL, PANEL_BORDER_STYLE_NONE);
+    title_panel->text = title_text;
+    title_panel->width = title_width;
+    title_panel->height = title_height;
+    add_panel_to_gui_screen(menu_gui_screen, title_panel);
 
-    const char* startText = "Press Enter to start.";
-    int startWidth, startHeight;
-    measure_text(startText, &startWidth, &startHeight);
+    const char* start_text = "Press Enter to start.";
+    int start_width, start_height;
+    measure_text(start_text, &start_width, &start_height);
 
-    struct Panel* startPanel = create_panel(NULL, PANEL_BORDER_STYLE_NONE);
-    startPanel->text = startText;
-    startPanel->width = startWidth;
-    startPanel->height = startHeight;
-    add_panel_to_gui_screen(menu_gui_screen, startPanel);
+    struct Panel* start_panel = create_panel(NULL, PANEL_BORDER_STYLE_NONE);
+    start_panel->text = start_text;
+    start_panel->width = start_width;
+    start_panel->height = start_height;
+    add_panel_to_gui_screen(menu_gui_screen, start_panel);
 
-    const char* helpText = "Press ? at any time for help.";
-    int helpWidth, helpHeight;
-    measure_text(helpText, &helpWidth, &helpHeight);
+    const char* help_text = "Press ? at any time for help.";
+    int help_width, help_height;
+    measure_text(help_text, &help_width, &help_height);
 
-    struct Panel* helpPanel = create_panel(NULL, PANEL_BORDER_STYLE_NONE);
-    helpPanel->text = helpText;
-    helpPanel->width = helpWidth;
-    helpPanel->height = helpHeight;
-    add_panel_to_gui_screen(menu_gui_screen, helpPanel);
+    struct Panel* help_panel = create_panel(NULL, PANEL_BORDER_STYLE_NONE);
+    help_panel->text = help_text;
+    help_panel->width = help_width;
+    help_panel->height = help_height;
+    add_panel_to_gui_screen(menu_gui_screen, help_panel);
 
     //  Center title
     SDL_Rect viewport;
     get_viewport(&viewport);
-    titlePanel->x = (viewport.w / 2) - (titlePanel->width / 2);
-    titlePanel->y = (viewport.h / 2) - (titlePanel->height / 2);
+    title_panel->x = (viewport.w / 2) - (title_panel->width / 2);
+    title_panel->y = (viewport.h / 2) - (title_panel->height / 2);
 
-    startPanel->x = (viewport.w / 2) - (startPanel->width / 2);
-    startPanel->y = titlePanel->y + titlePanel->height + 16;
+    start_panel->x = (viewport.w / 2) - (start_panel->width / 2);
+    start_panel->y = title_panel->y + title_panel->height + 16;
 
-    helpPanel->x = (viewport.w / 2) - (helpPanel->width / 2);
-    helpPanel->y = viewport.y + viewport.h - startPanel->height - 8;
+    help_panel->x = (viewport.w / 2) - (help_panel->width / 2);
+    help_panel->y = viewport.y + viewport.h - start_panel->height - 8;
 
     add_gui_screen(menu_gui_screen);
 }
